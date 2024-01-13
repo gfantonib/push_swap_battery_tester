99716da798b87a4a8b07c469f31b0991d9bfcafb
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-01-13 14:20:21 -0300

    time and moves formated

--- mia_combeau/test.py
+++ mia_combeau/test.py
@@ -3,14 +3,14 @@ import random
 
 # Random number generator
 randomlist = []
-for i in range(0,10):
+for i in range(0, 2000):
 	randomlist.append(i)
 random.shuffle(randomlist)
 numbers = ' '.join(map(str, randomlist))
 
 # Get the exec time of ./push_swap
-command = f"./push_swap {numbers}"
-moves = subprocess.run(
+command = f"(time ./push_swap {numbers})"
+moves_and_time = subprocess.run(
 command, \
 shell=True, \
 check=True, \
@@ -18,9 +18,18 @@ stdout=subprocess.PIPE, \
 stderr=subprocess.PIPE, \
 text=True)
 
-print("Command Output:")
-print(moves.stdout)
+# Format moves
+print("Moves output:")
+moves = moves_and_time.stdout
+moves = moves.replace('\n', ' ')
+moves = len(moves.split())
+print(moves)
 
-print("Errors:")
-print(moves.stderr)
+# Format time
+print("Time output:")
+time = moves_and_time.stderr
+time = time.split()
+time = time[2]
+time = time[0:7]
+print(time)
 
